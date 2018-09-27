019a8311b43ed4e33010e8f93e600fa4e1231d13
Mike Shaw <mike.shaw@dwp.gsi.gov.uk>
2018-09-27 11:40:26 +0100

    feat: add rules and tests for new standard
    Signed-off-by: Mike Shaw <mike.shaw@dwp.gsi.gov.uk>

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  root: true,
+  extends: ['eslint:recommended', 'airbnb-base'],
+  env: {
+    es6: true,
+  },
+  rules: {
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    curly: ['error', 'all'],
+    indent: ['error', 2],
+    semi: ['error', 'always', { omitLastInOneLineBlock: false }],
+    'valid-jsdoc': 'error',
+  },
+};
