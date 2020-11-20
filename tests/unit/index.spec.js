20b6411dad803465f69418ec45f873157de383ff
Jayne Doe <jayne.doe@engineering.digital.dwp.gov.uk>
2020-11-20 15:24:23 +0000

    feat(jsdoc): add jsdoc plugin and opinionated ruleset
    Signed-off-by: Jayne Doe <jayne.doe@engineering.digital.dwp.gov.uk>

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -14,6 +14,10 @@ describe('@dwp/eslint-config-base', () => {
       expect(eslintConfigKeys).to.contain('parserOptions');
     });
 
+    it('should have a plugins key', () => {
+      expect(eslintConfigKeys).to.contain('plugins');
+    });
+
     it('should have a root key', () => {
       expect(eslintConfigKeys).to.contain('parserOptions');
     });
@@ -31,7 +35,7 @@ describe('@dwp/eslint-config-base', () => {
     });
 
     it('not have any other keys', () => {
-      expect(eslintConfigKeys).to.deep.equal(['parserOptions', 'root', 'extends', 'env', 'rules']);
+      expect(eslintConfigKeys).to.deep.equal(['parserOptions', 'plugins', 'root', 'extends', 'env', 'rules']);
     });
 
     describe('the parserOptions key should', () => {
@@ -83,7 +87,7 @@ describe('@dwp/eslint-config-base', () => {
       const rule = eslintConfig.extends;
 
       it('inherit recommended settings', () => {
-        expect(rule).to.deep.equal(['eslint:recommended', 'airbnb-base']);
+        expect(rule).to.deep.equal(['eslint:recommended', 'plugin:jsdoc/recommended', 'airbnb-base']);
       });
     });
 
@@ -136,12 +140,71 @@ describe('@dwp/eslint-config-base', () => {
         expect(Object.keys(rules)).to.contain('semi');
       });
 
-      it('have a valid-jsdoc key', () => {
-        expect(Object.keys(rules)).to.contain('valid-jsdoc');
+      describe('jsdoc-specific rules should', () => {
+        it('check tag names are valid', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/check-tag-names');
+        });
+
+        it('require a description', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-description');
+        });
+
+        it('require descriptions are complete sentences', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-description-complete-sentence');
+        });
+
+        it('require parameters to be documented', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-param');
+        });
+
+        it('require parameters to have a description', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-param-description');
+        });
+
+        it('require parameters to have a name', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-param-name');
+        });
+
+        it('require parameters to have a type', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-param-type');
+        });
+
+        it('require returns to be documented', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-returns');
+        });
+
+        it('require returns tags to have a corresponding return statement', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-returns-check');
+        });
+
+        it('require returns to have a description', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-returns-description');
+        });
+
+        it('require returns to have a type', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-returns-type');
+        });
+
+        it('require throws to be documented', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-throws');
+        });
+
+        it('requires jsdocs to be created', () => {
+          expect(Object.keys(rules)).to.contain('jsdoc/require-jsdoc');
+        });
       });
 
       it('should not have any other keys', () => {
-        expect(Object.keys(rules)).to.deep.equal(['brace-style', 'curly', 'indent', 'no-plusplus', 'semi', 'valid-jsdoc']);
+        expect(Object.keys(rules)).to.deep.equal([
+          'brace-style', 'curly', 'indent', 'no-plusplus', 'semi',
+          'jsdoc/check-tag-names', 'jsdoc/require-description',
+          'jsdoc/require-description-complete-sentence',
+          'jsdoc/require-param', 'jsdoc/require-param-description',
+          'jsdoc/require-param-name', 'jsdoc/require-param-type',
+          'jsdoc/require-returns', 'jsdoc/require-returns-check',
+          'jsdoc/require-returns-description', 'jsdoc/require-returns-type',
+          'jsdoc/require-throws', 'jsdoc/require-jsdoc',
+        ]);
       });
 
       describe('the brace-style key should', () => {
@@ -228,15 +291,203 @@ describe('@dwp/eslint-config-base', () => {
         });
       });
 
-      describe('the valid-jsdoc key should', () => {
-        const rule = rules['valid-jsdoc'];
+      describe('the jsdoc ruleset, containing', () => {
+        describe('the check-tag-names key should', () => {
+          const rule = rules['jsdoc/check-tag-names'];
 
-        it('raise an error if not applied', () => {
-          expect(rule).to.equal('error');
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
         });
 
-        it('and not do anything else', () => {
-          expect(rule).to.equal('error');
+        describe('the require-description key should', () => {
+          const rule = rules['jsdoc/require-description'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-description-complete-sentence key should', () => {
+          const rule = rules['jsdoc/require-description-complete-sentence'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-param key should', () => {
+          const rule = rules['jsdoc/require-param'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-param-description key should', () => {
+          const rule = rules['jsdoc/require-param-description'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-param-name key should', () => {
+          const rule = rules['jsdoc/require-param-name'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-param-type key should', () => {
+          const rule = rules['jsdoc/require-param-type'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-returns key should', () => {
+          const rule = rules['jsdoc/require-returns'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and force returns for asynchronous functions', () => {
+            expect(rule[1]).to.deep.equal({ forceReturnsWithAsync: true });
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error', { forceReturnsWithAsync: true }]);
+          });
+        });
+
+        describe('the require-returns-check key should', () => {
+          const rule = rules['jsdoc/require-returns-check'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-returns-description key should', () => {
+          const rule = rules['jsdoc/require-returns-description'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-returns-type key should', () => {
+          const rule = rules['jsdoc/require-returns-type'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-throws key should', () => {
+          const rule = rules['jsdoc/require-throws'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error']);
+          });
+        });
+
+        describe('the require-jsdoc key should', () => {
+          const rule = rules['jsdoc/require-jsdoc'];
+
+          it('raise an error', () => {
+            expect(rule[0]).to.equal('error');
+          });
+
+          it('and include class properties within its contexts', () => {
+            expect(rule[1].contexts).to.contain('ClassProperty');
+          });
+
+          it('and require that arrow functions have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('ArrowFunctionExpression');
+          });
+
+          it('and require that class declarations have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('ClassDeclaration');
+          });
+
+          it('and require that class expressions have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('ClassExpression');
+          });
+
+          it('and require that function declarations have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('FunctionDeclaration');
+          });
+
+          it('and require that function expressions have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('FunctionExpression');
+          });
+
+          it('and require that method definitions have a jsdoc block', () => {
+            expect(Object.keys(rule[1].require)).to.contain('MethodDefinition');
+          });
+
+          it('and do nothing else', () => {
+            expect(rule).to.deep.equal(['error', {
+              contexts: ['ClassProperty'],
+              require: {
+                ArrowFunctionExpression: true,
+                ClassDeclaration: true,
+                ClassExpression: true,
+                FunctionDeclaration: true,
+                FunctionExpression: true,
+                MethodDefinition: true,
+              },
+            }]);
+          });
         });
       });
     });
